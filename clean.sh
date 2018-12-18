3bc1bfecc64cb7f202809f40707dd1c16b0baccc
Umanga Chapagain <chapagainumanga@gmail.com>
2018-12-18 18:55:39 +0530

    Removed dashboard_out from clean.sh
    Signed-off-by: Umanga Chapagain <chapagainumanga@gmail.com>

--- clean.sh
+++ clean.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 echo 'Removing prometheus_alerts.yaml'
 rm prometheus_alerts.yaml
-echo 'Removing dashboards_out/*'
-rm -rf dashboards_out
 echo 'Removing prometheus_rules.yaml'
 rm prometheus_rules.yaml
 echo 'Finished. Cleaned.'
