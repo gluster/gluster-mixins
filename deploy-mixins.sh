f68234e1324b7c6713f976b83e8ba73c95f768a1
Umanga Chapagain <chapagainumanga@gmail.com>
2018-12-26 11:36:36 +0530

    Updated deploy-mixins.sh
    Signed-off-by: Umanga Chapagain <chapagainumanga@gmail.com>

--- deploy-mixins.sh
+++ deploy-mixins.sh
@@ -4,9 +4,9 @@ set -e
 set -x
 set -o pipefail
 
-git clone https://github.com/gluster/gluster-mixins.git
+#git clone https://github.com/gluster/gluster-mixins.git
+git clone -b kube-prometheus https://github.com/umangachapagain/gluster-mixins.git
 cd gluster-mixins/extras
 jb install
 sh build.sh
-
 kubectl apply -f manifests/
